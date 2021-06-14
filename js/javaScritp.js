782c3c28f8deb17471e0b5a1a9a77027f0f5d457
achrafOuakka <ouakka.achraf@gmail.com>
2021-06-15 00:47:08 +0100

    faire style sur liste d'images

--- js/javaScritp.js
+++ js/javaScritp.js
@@ -6,3 +6,18 @@
         $($(this).data("class")).siblings().hide().end().show();
 
 });
+
+
+    /***** Start why this is awesome section ******/
+    $( ".awesome .card" ).hover(
+        function() {
+            $(this).removeClass('shadow-sm');
+            $(this).addClass('shadow').css('cursor', 'pointer');
+        }, function() {
+            $(this).removeClass('shadow');
+            $(this).addClass('shadow-sm');
+        }
+    );
+    /***** End why this is awesome section ******/
+
+    $(".year").text(new Date().getFullYear());
