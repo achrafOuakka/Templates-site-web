6177beb3bd4edf5dacdbaad92860193270f11027
achrafOuakka <ouakka.achraf@gmail.com>
2021-06-19 20:30:53 +0100

    fait la section Footer et Contact et Stylish Saying

--- js/javaScritp.js
+++ js/javaScritp.js
@@ -1,9 +1,12 @@
 
-    $(".tables ul li").click(function () {
-
+/***** Start Tables section ******/
+$(".tables ul li").click(function () {
+        //remove in ly siblinges and add class in me
         $(this).siblings().removeClass("active").end().addClass("active");
         console.log($(this).data("class"));
         $($(this).data("class")).siblings().hide().end().show();
+
+        $($(this).data("class")).siblings().removeClass("d-flex d-md-block").end().addClass("d-flex d-md-block");
 
 });
 
@@ -18,17 +21,18 @@
             $(this).addClass('shadow-sm');
         }
     );
-    /***** End why this is awesome section ******/
-    $( ".saying .card" ).hover(
-        function() {
-            $(this).removeClass('border-bottom');
-            $(this).addClass('border-bottom-card').css('cursor', 'pointer');
-        }, function() {
-            $(this).addClass('border-bottom');
-            $(this).removeClass('border-bottom-card');
-        }
-    );
-    /****** Start saying section ******/
 
-    /***** footer ******/
+    /****** Start saying section ******/
+    // $( ".saying .card" ).hover(
+    //     function() {
+    //         // $(this).removeClass('border-bottom');
+    //         $(this).addClass('border-bottom-card').css('cursor', 'pointer');
+    //     }, function() {
+    //         // $(this).addClass('border-bottom');
+    //         $(this).removeClass('border-bottom-card');
+    //     }
+    // );
+
+
+    /***** Start footer ******/
     $(".year").text(new Date().getFullYear());
